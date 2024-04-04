3a7d5730417317eb27ece3da481a6ad89eb4f1b0
Pedro Tavares <ph_tavares@hotmail.com>
2024-04-04 09:20:00 -0300

    Otimização com arrays

--- src/LancamentoDados.java
+++ src/LancamentoDados.java
@@ -9,12 +9,7 @@ public class LancamentoDados {
         SecureRandom randomNumbers = new SecureRandom();
         Scanner input = new Scanner(System.in);
 
-        int frequency1 = 0;
-        int frequency2 = 0;
-        int frequency3 = 0;
-        int frequency4 = 0;
-        int frequency5 = 0;
-        int frequency6 = 0;
+        int[] frequency = new int[7];
 
         System.out.print("Digite o número de vezes que deseja lançar o dado: ");
         int n = input.nextInt();
@@ -23,38 +18,18 @@ public class LancamentoDados {
 
             int numero = 1 + randomNumbers.nextInt(6);
 
-            switch (numero) {
-
-                case 1:
-                    ++frequency1;
-                    break;
-                case 2:
-                    ++frequency2;
-                    break;
-                case 3:
-                    ++frequency3;
-                    break;
-                case 4:
-                    ++frequency4;
-                    break;
-                case 5:
-                    ++frequency5;
-                    break;
-                case 6:
-                    ++frequency6;
-                    break;
-
-            }
+            ++frequency[numero];
 
         }
 
         System.out.println("Frequência dos números: ");
-        System.out.println("1: " + frequency1);
-        System.out.println("2: " + frequency2);
-        System.out.println("3: " + frequency3);
-        System.out.println("4: " + frequency4);
-        System.out.println("5: " + frequency5);
-        System.out.println("6: " + frequency6);
+        System.out.println("1: " + frequency[1]);
+        System.out.println("2: " + frequency[2]);
+        System.out.println("3: " + frequency[3]);
+        System.out.println("4: " + frequency[4]);
+        System.out.println("5: " + frequency[5]);
+        System.out.println("6: " + frequency[6]);
+
 
     }
 
